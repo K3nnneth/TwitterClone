f76d8724e371c658acc803613d4d16272feaab47
K3nnneth <K.bui2@yahoo.com>
2022-09-30 18:07:04 -0700

    finished twitter pt 1

--- twitter_ios_starter/Twitter/TwitterAPICaller.swift
+++ twitter_ios_starter/Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "0HDLZT1EWfA2yo0PSUR1xDCCC", consumerSecret: "qxLIPIB4otNnjoRPvVc6fZ9WDZNMiJKDXLoQjRhQ24TcEBgpR1")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
